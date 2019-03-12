defebf06cbfab18c8f9153827e75064ca8c2fc62
Varun Kudalkar <varun.kudalkar@gmail.com>
2019-03-12 19:01:24 +0530

    first commit

---
+++ DataTypes.py
@@ -0,0 +1,29 @@
+# premitive
+
+# 1) int
+
+my_int = 10
+
+print("Value of int: %d"% my_int)
+# 2) float
+
+my_float = 10.28
+
+print("Value of float: %f"% my_float)
+
+# 3) str
+
+my_str = "Codekul"
+my_str = 'Codekul'
+
+# 'Codekul' - "The Gurukul for Coders!"
+
+my_str = """'Codekul' - "The Gurukul for Coders!" """
+
+print("Value of str: %s"% my_str)
+
+# 4) bool
+
+my_bool = True
+
+print("my_bool: {}".format(my_bool))
---
+++ VariablesAndConst.py
@@ -0,0 +1,21 @@
+import Constants
+
+var = "Codekul"
+var = "Python"
+a1_sd = 10
+
+# Constants.PI = 3.14798 
+
+"""
+this is 
+multiline
+comment
+"""
+
+# to print value of PI
+print(Constants.PI)
+# to print value of gravity
+print(Constants.GRAVITY)
+
+
+print(var)
